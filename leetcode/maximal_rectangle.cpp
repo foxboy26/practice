ef9fec123bf1c4d39e1f294f74c32cf6a415951e
Zhiheng Li <lizhiheng.sjtu@gmail.com>
2013-07-25 20:54:25 -0700

    rename

--- leetcode/maximal_rectangle.cpp
+++
@@ -1,9 +0,0 @@
-int maximalRectangle(vector<vector<char> > &matrix) {
-  // Start typing your C/C++ solution below
-  // DO NOT write int main() function
-  
-  int m = matrix.size(), n = matrix[0].size();
-
-
-
-}
